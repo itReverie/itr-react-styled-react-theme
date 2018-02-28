27e8553152a4e6295edd67589b07c95509b7b96b
itReverie <brenda@itreverie.com>
2018-02-28 14:37:55 -0500

    Adding media queries

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import styled from 'styled-components';
 import theme from '../../styles/theme';
+import media from '../../styles/media';
 
 import Profile from './profile';
 import RoleSelector from './roleSelector';
 import Metric from './metric';
 
+ console.log(theme);
 const StyledHeader= styled.div`
                     display: flex;
                     flex-direction: row;
@@ -14,6 +16,8 @@ const StyledHeader= styled.div`
                     align-items: center;
                     background-color: ${theme.dark};
                     color:${theme.light};
+                    ${media.tablet`flex-direction: column;`}
+                    ${media.phone`flex-direction: column;`}
                     `;
 
 export default class Header extends Component {
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,12 +1,11 @@
 import createTheme from 'styled-components-theme';
 import {injectGlobal}  from 'styled-components';
-import colors from './colors'
-import fonts from './fonts'
+import colors from './colors';
+import fonts from './fonts';
+import media from './media';
 
 
-
-const theme = createTheme(...Object.keys(colors , fonts))
-
+const theme = createTheme(...Object.keys(colors))
 
 injectGlobal`
 body {
