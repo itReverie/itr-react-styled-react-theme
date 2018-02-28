b099cc8adc5ed4d3d7efe3fa925f207124c03cbe
itReverie <brenda@itreverie.com>
2018-02-28 11:16:27 -0500

    Adding the header

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,33 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as conceptActions from '../actions/conceptActions';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+
+const StyledHeader= styled.div`
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between ;
+                    align-items: center;
+                    `;
+
+const Button = styled.button`
+                      font-size: 1em;
+                      margin: 1em;
+                      padding: 0.25em 1em;
+                      border-radius: 3px;
+                      color: ${props => props.theme.main};
+                      border: 2px solid ${props => props.theme.main};
+                      `;
+
+                    // We're passing a default theme for Buttons that aren't wrapped in the ThemeProvider
+                    Button.defaultProps = {
+                      theme: {
+                        main: 'palevioletred'
+                      }
+                    }
+
+
 
 
 class App extends Component {
@@ -10,7 +37,14 @@ class App extends Component {
 
 
   render() {
-    return <div>Hello</div>;
+    return (<StyledHeader>
+                 <div>Welcome Charles</div>
+                 <select id = "myDashboard">
+                  <option value = "1">Accountant</option>
+                  <option value = "2">HR</option>
+                 </select>
+                 <Button>Add metrics</Button>
+           </StyledHeader>);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ import './index.css';
 import '../node_modules/highlight.js/styles/atom-one-dark.css';
 import Routes from './routes';
 import {loadConcepts} from './actions/conceptActions';
+import {ThemeProvider} from 'styled-components';
 
 const store=configureStore();
+
+// Define what props.theme will look like
+const theme = {
+  main: 'mediumseagreen'
+};
 
 //Dispatch Actions
 store.dispatch(loadConcepts());
 
 ReactDOM.render(
   (<Provider store={store}>
+    <ThemeProvider theme={theme}>
     <Routes />
+    </ThemeProvider>
   </Provider>),
   document.getElementById('root'));
